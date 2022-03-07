7149961e8e3610ad7e61fc324f8425bc3bba1e61
kamil <almetov.kamil@gmail.com>
2022-03-07 13:30:52 +0300

    Exercises 1 is fixed

--- ex01.hpp
+++ ex01.hpp
@@ -5,8 +5,8 @@
 template <typename T>
 void multiples (T& sum, T x, int n)
 {
-    sum = x;
-    for (int i = 2; i <= n; ++i) {
+    sum = 1;
+    for (int i = 1; i <= n; ++i) {
         sum += i * x;
     }
 }
